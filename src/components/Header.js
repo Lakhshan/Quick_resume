85793e0f6147fb83738febcb5ec7e686a8c79931
Lakhshan Saravanan <Bhalage123@gmail.com>
2024-09-08 14:20:46 +0530

    6th commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
-// import resumeLogo from "../assets/icons/resumeLogo.png";
+import resumeLogo from "../imgs/Quick Résumé.webp";
 export function Header() {
   return (
     < >
       <div className="w-[200px] shadow-lg shadow-gray-400 flex flex-col items-center h-[400px] rounded-lg bg-white"  style={{boxShadow: '0px 0px 10px 0px rgba(0,0,0,0.75)'}}>
-        {/* <img src={resumeLogo} alt="Logo " width={"100px"} className="cursor-pointer"/> */}
+        <img src={resumeLogo} alt="Logo " width={"100px"} className="cursor-pointer m-10"/>
         <h1 className="font-bold font-serif">RESUME</h1>
         <h2 className="font-bold font-serif">GENERATOR</h2>
         <div className="mt-[200px] mb-10">
--- src/pages/template/tempsele.js
+++ src/pages/template/tempsele.js
@@ -1,19 +1,33 @@
-import React,{ useState } from 'react';
-import {Grid, Paper, Typography, Button } from '@mui/material';
+import React, { useState } from 'react';
+import { Grid, Paper, Typography, Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
-
-
+import temp1img from "../../imgs/temp1.png"
+import soon from "../../imgs/comingsoon.jpg"
 
 export function TemplateSelection() {
-  const templates = ['Template 1', 'Template 2', 'Template 3']; // Example template names
+  const templates = [
+    {
+      name: 'Template 1',
+      image: temp1img,
+    },
+    {
+      name: 'Template 2',
+      image: soon,
+    },
+    {
+      name: 'Template 3',
+      image: soon,
+    },
+  ];
+
   const [selectedTemplate, setSelectedTemplate] = useState(null);
   const navigate = useNavigate();
 
   const handleSelectTemplate = (template) => {
-    setSelectedTemplate(template);
-    navigate(`/templates/${template.toLowerCase().replace(' ', '')}`);
+    setSelectedTemplate(template.name);
+    navigate(`/templates/${template.name.toLowerCase().replace(' ', '')}`);
   };
-  
+
   return (
     <Grid container spacing={2} sx={{ padding: 4 }}>
       {templates.map((template, index) => (
@@ -26,7 +40,12 @@ export function TemplateSelection() {
               alignItems: 'center',
             }}
           >
-            <Typography variant="h6">{template}</Typography>
+            <img
+              src={template.image}
+              alt={template.name}
+              style={{ width: '100%', height: 'auto', marginBottom: 16 }}
+            />
+            <Typography variant="h6">{template.name}</Typography>
             <Button variant="contained" sx={{ mt: 2 }} onClick={() => handleSelectTemplate(template)}>
               Select
             </Button>
